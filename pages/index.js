ab39e0da95159281fa376f958b2f817fe18a6b71
Loan Matteus <loan.matteusz@gmail.com>
2021-03-05 02:15:24 -0300

    chore: add components of the day one

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import QuizBackground from '../src/components/QuizBackground'
+import Widget from '../src/components/Widget'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Footer from '../src/components/Footer'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <QuizBackground backgroundImage={ db.bg[Math.floor(Math.random() * db.bg.length)] }>
+        <QuizContainer>
+
+          <Widget>
+            <Widget.Header>
+              <h1>Valorant</h1>
+            </Widget.Header>
+
+            <Widget.Content>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+            </Widget.Content>
+          </Widget>
+          
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </p>
+            </Widget.Content>
+          </Widget>
+
+          <Footer />
+        </QuizContainer>
+
+        <GitHubCorner projectUrl="https://github.com/loanmatteusz"/>
+      </QuizBackground>
+    </>
+  )
 }
